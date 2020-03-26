6825246b7044f76780efd59f7ed4b5af9ab7291a
stefananbeek <nafets.a@hotmail.com>
2020-03-26 19:19:18 +0100

    Comment

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -32,7 +32,7 @@ public class ASTListener extends ICSSBaseListener {
     }
 
     // OVERRIDING
-    // Parser tm level 3 werkt, rest van de code tot level 2
+    // Parser tm level 3 werkt
     @Override
     public void enterStylesheet(final ICSSParser.StylesheetContext ctx) {
         Stylesheet stylesheet = new Stylesheet();
